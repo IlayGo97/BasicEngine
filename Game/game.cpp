d0bb2bf57694454d72b786a348f50d974f1629ef
kingOren <orenpartovi@gmail.com>
2022-11-08 15:18:56 +0200

    added lena texture

--- Game/game.cpp
+++ Game/game.cpp
@@ -27,8 +27,8 @@ void Game::Init()
 	AddShader("../res/shaders/pickingShader");	
 	AddShader("../res/shaders/basicShader");
 	
-	AddTexture("../res/textures/box0.bmp",false);
-
+//	AddTexture("../res/textures/box0.bmp",false);
+    AddTexture("../res/textures/lena256.jpg", false);
 	AddShape(Plane,-1,TRIANGLES);
 	
 	pickedShape = 0;
